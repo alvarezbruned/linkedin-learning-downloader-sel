364bedb55d05ab8974592cec3fc6b238cba8a85a
albertalvarez <albert@alvarezbruned.com>
2019-07-15 06:40:28 +0200

    First commit with first project status

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # linkedin-learning-downloader-sel
+
+
+To run script your credentials and courses go inside the code (in the future it will be a yaml file with credentials and courses)
+
+The courses in code are an array, taken the last part of URL:
+example:
+https://www.linkeidn.com/learning/python-para-data-science-y-big-data-esencial
+in code only write 'python-para-data-science-y-big-data-esencial'
+
+The baseVideoPath is necessary change, is the destiny of videos courses, this in future it will be as a variable in yaml file.
+
+## Necessary installs:
+pip3 install -r requirements.txt
+
+## Driver geckodriver is needed
+wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+tar -xvzf geckodriver*
+mv geckodriver /usr/local/bin
+rm geckodriver*
---
+++ linkedin.py
@@ -0,0 +1,127 @@
+import os
+import time
+import urllib.request
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+def is_in_page(selector):
+    try:
+        if "@" in selector:
+            return driver.find_element_by_xpath(selector)
+        return driver.find_element_by_css_selector(selector)
+    except Exception:
+        print(selector + ' Not found')
+        return False
+
+
+def click_if_exist(path_locator):
+    element = is_in_page(path_locator)
+    if element:
+        element.click()
+        return element
+    return False
+
+
+def type_if_exist(path_locator, keys_to_send):
+    element = click_if_exist(path_locator)
+    if element:
+        element.send_keys(keys_to_send)
+
+
+def active_action(action):
+    start_button = 'button#btn-start-workday'
+    stop_button = 'button#btn-stop-workday'
+    lunch_button = 'button#btn-pause-lunch'
+    come_back_from_lunch_button = 'button#btn-resume-workday'
+    confirm_button = 'button.swal2-confirm.swal2-styled'
+    switch = {
+        'start' : start_button,
+        'stop' : stop_button,
+        'lunch' : lunch_button,
+        'comBackFromLunch' : come_back_from_lunch_button
+    }
+    click_if_exist(switch.get(action, 'error'))
+    time.sleep(2)
+    click_if_exist(confirm_button)
+
+
+# driver = webdriver.Remote("firefox:4444")
+driver = webdriver.Firefox()
+time.sleep(5)
+driver.get("https://www.linkedin.com/learning/login?redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Flearning%2F&trk=sign_in&upsellOrderOrigin=trk_default_learning")
+driver.maximize_window()
+try:
+    for iframe in driver.find_elements_by_tag_name("iframe"):
+        driver.switch_to.frame(iframe)
+        if is_in_page('input#username'):
+            break
+        else:
+            driver.switch_to.default_content()
+except:
+    print('no se puede cambiar a iframe')
+# driver.switch_to.frame(driver.find_elements_by_tag_name("iframe")[0])
+
+type_if_exist('input#username', 'user@mail')
+time.sleep(3)
+type_if_exist('input#password', 'password')
+time.sleep(4)
+type_if_exist('input#password', Keys.ENTER)
+time.sleep(8)
+driver.switch_to.default_content()
+courses = [
+    'python-para-data-science-y-big-data-esencial',
+    'istio-y-kubernetes-esencial',
+    'kubernetes-para-desarrolladores-esencial',
+    'kubernetes-para-administradores-it-esencial',
+]
+baseVideoPath = '/home/user/path'
+for course in courses:
+    time.sleep(8)
+    numVideo = 1
+    print('https://www.linkedin.com/learning/' + course)
+    driver.get('https://www.linkedin.com/learning/' + course)
+    time.sleep(10)
+    if is_in_page('.course-toc__item-content.t-14.t-black.t-normal') == False:
+        driver.get('https://www.linkedin.com/learning/' + course)
+        time.sleep(15)
+    try:
+        os.mkdir(baseVideoPath + '/' + course)
+    except:
+        print('no se puede crear dir')
+    try:
+        for iframe in driver.find_elements_by_tag_name("iframe"):
+            driver.switch_to.frame(iframe)
+            if is_in_page('.course-toc__item-content.t-14.t-black.t-normal'):
+                break
+            else:
+                driver.switch_to.default_content()
+    except:
+        print('no se puede cambiar a iframe')
+    if is_in_page('.course-toc__item-content.t-14.t-black.t-normal'):
+        videos = driver.find_elements_by_css_selector('.course-toc__item-content.t-14.t-black.t-normal')
+        for video in videos:
+            title = str(video.text)
+            title = title.replace('/', '-')
+            title = title.replace('\\', '-')
+            print(title)
+            video.click()
+            time.sleep(8)
+            mp4 = ''
+            if is_in_page('.vjs-tech'):
+                mp4 = is_in_page('.vjs-tech').get_attribute('src')
+                print(mp4)
+                urllib.request.urlretrieve(mp4, baseVideoPath + '/' + course + '/' + str(numVideo) + '-' + title + '.mp4')
+            else:
+                video.click()
+                time.sleep(12)
+                if is_in_page('.vjs-tech'):
+                    mp4 = is_in_page('.vjs-tech').get_attribute('src')
+                    print(mp4)
+                    urllib.request.urlretrieve(mp4, baseVideoPath + '/' + course + '/' + str(numVideo) + '-' + title + '.mp4')
+            time.sleep(15)
+            numVideo = numVideo + 1
+    driver.switch_to.default_content()
+
+driver.close()
